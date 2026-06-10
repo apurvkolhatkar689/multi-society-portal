741b9d461ec31e200468e4f5c165621e7e6127ae
BSujay <sujaybapat@outlook.com>
2026-06-10 17:05:17 +0530

    Created Registration form

--- main.go
+++ main.go
@@ -24,6 +24,15 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, data)
 }
 
+func registrationHandler(w http.ResponseWriter, r *http.Request) {
+	selectedSociety := r.URL.Query().Get("society")
+	data := LoginPageData{
+		Society: selectedSociety,
+	}
+	tmpl := template.Must(template.ParseFiles("templates/registration.html"))
+	tmpl.Execute(w, data)
+}
+
 func main() {
 
 	http.Handle("/static/",
@@ -32,6 +41,7 @@ func main() {
 
 	http.HandleFunc("/", selectSocietyHandler)
 	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/registration", registrationHandler)
 
 	log.Println("Server started at :8080")
 	http.ListenAndServe(":8080", nil)
